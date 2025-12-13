4b02ba4b0093aba703831885abcaf102a0d58f8d
Seba Cardoso <hi@sebastiancardoso.com>
2025-12-13 18:48:23 -0300

    feat: Implement modal close button and enhance modal title and description styles.

--- src/components/ui/modal/modal.tsx
+++ src/components/ui/modal/modal.tsx
@@ -35,6 +35,22 @@ export default component$<ModalProps>((props) => {
       </Modal.Trigger>
 
       <Modal.Panel class="modal-panel modal-animation">
+        <Modal.Close class="modal-close-icon">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path d="M18 6 6 18" />
+            <path d="m6 6 12 12" />
+          </svg>
+        </Modal.Close>
         <Modal.Title>{props.title}</Modal.Title>
         <Modal.Description>{props.description}</Modal.Description>
         <Slot />
